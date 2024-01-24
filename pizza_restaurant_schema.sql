2d420a306796f5f1127b5a2cf3b3b6c95c61f2df
MiriamLaw <mimsical999@yahoo.com>
2024-01-23 18:39:27 -0800

    Added initial database creation script

---
+++ pizza_restaurant_schema.sql
@@ -0,0 +1,37 @@
+USE PizzaRestaurant;
+
+CREATE TABLE Customers (
+customer_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_name VARCHAR(255) NOT NULL,
+phone_number VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Orders (
+order_id INT AUTO_INCREMENT PRIMARY KEY,
+customer_id INT NOT NULL,
+order_date_time DATETIME NOT NULL,
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) 
+);
+
+CREATE TABLE Pizzas (
+pizza_id INT AUTO_INCREMENT PRIMARY KEY,
+pizza_type VARCHAR(255) NOT NULL,
+price DECIMAL(5, 2) NOT NULL
+);
+
+CREATE TABLE Customer_Orders(
+customer_id INT NOT NULL,
+order_id INT NOT NULL,
+PRIMARY KEY (customer_id, order_id),
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+);
+
+CREATE TABLE Pizzas_Orders (
+order_id INT NOT NULL,
+pizza_id INT NOT NULL,
+quantity INT NOT NULL,
+PRIMARY KEY (order_id, pizza_id),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (pizza_id) REFERENCES Pizzas(pizza_id)
+);
